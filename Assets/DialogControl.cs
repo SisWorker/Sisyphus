a4a85415ecb3f98e7fe5114feafb8fcc5811fe44
Yifan <Ekho@LiyifandeMacBook-Pro.local>
2015-07-13 16:51:47 +0800

    add Dialog Box

---
+++ Assets/DialogControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogControl : MonoBehaviour {
+
+	public bool dialogOn;
+	public GameObject DialogCanvas;
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {
+		if (dialogOn) {
+			DialogCanvas.SetActive (true);
+		} else {
+			DialogCanvas.SetActive (false);
+		}
+	
+	}
+	void showDialog()
+	{
+		dialogOn = true;
+	}
+	void hideDialog()
+	{
+		dialogOn = false;
+	}
+}
--- Assets/Script/UIDialog.cs
+++ Assets/Script/UIDialog.cs
@@ -3,15 +3,42 @@ using System.Collections;
 using UnityEngine.UI;
 public class UIDialog : MonoBehaviour {
 
-	public string dialogString;
+	public GameObject DialogBox;
+	private Text dialogText;
+	private int dialogCount;
+	private bool done;
 	// Use this for initialization
 	void Start ()
 	{
-		dialogString = GameObject.Find ("DialogText").GetComponent<Text> ().text;
+		dialogCount = -1;
+		dialogText = GameObject.Find ("DialogText").GetComponent<Text> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (dialogCount == 0) {
+			dialogText.text = "What";
+		}
+		else if (dialogCount == 1) {
+			dialogText.text = "What?";
+		}
+		else if (dialogCount == 2) {
+			dialogText.text = "What??";
+		}
+		else if (dialogCount == 3) {
+			dialogText.text = "What???";
+		}
+		else if (dialogCount == 4) {
+			dialogText.text = "What????";
+		} else {
+
+			DialogBox.SetActive(false);
+		}
 	
+	}
+	public void Proceed()
+	{
+		dialogCount++;
+
 	}
 }
