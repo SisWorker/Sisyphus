876f2a7e19018da555c7ac24e986ca453f63ac50
Yifan <Ekho@LiyifandeMacBook-Pro.local>
2015-07-21 16:12:09 +0800

    add button for platform

---
+++ Assets/Scripts/PlatformButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("RockContact")||other.gameObject.CompareTag ("Player"))
+		{
+			transform.GetComponent<SpriteRenderer>().enabled=false;
+			transform.GetComponentInParent<MovingPlatform>().MoveOn();
+		}
+
+	}
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("RockContact")||other.gameObject.CompareTag ("Player"))
+		{
+			transform.GetComponent<SpriteRenderer>().enabled=true;
+		}
+	}
+}
