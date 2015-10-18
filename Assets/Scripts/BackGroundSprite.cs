f611bf7bde4dc4b4ac9a809329b1bd4bae93808f
Boting Li <botingli27@gmail.com>
2015-10-18 13:40:00 -0400

    v0.2 fixed throw with Demo level

---
+++ Assets/Scripts/BackGroundSprite.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundSprite : MonoBehaviour {
+
+    private GameObject Camera;
+    private Camera cam;
+    private float relativeScale;
+    private float z;
+	// Use this for initialization
+	void Start () {
+        Camera = GameObject.Find("Main Camera");
+        cam = Camera.gameObject.GetComponent<Camera>();
+        relativeScale = transform.localScale.x / cam.orthographicSize;
+        z = transform.position.z;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(cam.transform.position.x,cam.transform.position.y,z);
+        transform.localScale = new Vector3(cam.orthographicSize * relativeScale, cam.orthographicSize * relativeScale, cam.orthographicSize * relativeScale);
+	}
+}
